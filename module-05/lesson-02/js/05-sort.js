ade9f5d2c8f9af3cf30036d02e3d920069d611a5
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2024-03-20 00:23:35 +0200

    Update 05-sort.js

--- module-05/lesson-02/js/05-sort.js
+++ module-05/lesson-02/js/05-sort.js
@@ -13,10 +13,10 @@
 const numbers = [1, 9, 6, 2, 3];
 
 const sorted = numbers;
-console.log("sorted ", sorted);
+console.log('sorted ', sorted);
 
-const letters = ["b", "B", "a", "A"];
-console.log("letters", letters);
+const letters = ['b', 'B', 'a', 'A'];
+console.log('letters', letters);
 
 /**
  * compareFunction - функція порівняння (callback)
@@ -31,27 +31,27 @@ console.log(
 
 const descSortedNumbers = numbers;
 const ascSortedNumbers = numbers;
-console.log("descSortedNumbers", descSortedNumbers);
-console.log("ascSortedNumbers", ascSortedNumbers);
+console.log('descSortedNumbers', descSortedNumbers);
+console.log('ascSortedNumbers', ascSortedNumbers);
 
 /**
  * Сортування масиву об'єктів
  */
 const players = [
-  { id: "player-1", name: "Mango", timePlayed: 310, online: false },
-  { id: "player-2", name: "Poly", timePlayed: 470, online: true },
-  { id: "player-3", name: "Aiwi", timePlayed: 230, online: true },
-  { id: "player-4", name: "Ajax", timePlayed: 150, online: false },
-  { id: "player-5", name: "Chelsey", timePlayed: 80, online: true },
+  { id: 'player-1', name: 'Mango', timePlayed: 310, online: false },
+  { id: 'player-2', name: 'Poly', timePlayed: 470, online: true },
+  { id: 'player-3', name: 'Aiwi', timePlayed: 230, online: true },
+  { id: 'player-4', name: 'Ajax', timePlayed: 150, online: false },
+  { id: 'player-5', name: 'Chelsey', timePlayed: 80, online: true },
 ];
 
-// За ігровим часом
+// За ігровим часом в порядку зростання / спадання
 const sortedByBestPlayers = players;
 console.table(sortedByBestPlayers);
 
 const sortedByWorstPlayers = players;
 console.table(sortedByWorstPlayers);
 
-// По первой букве имени
+// За першою літерою в імені в порядку зростання
 const byName = players;
 console.table(byName);
